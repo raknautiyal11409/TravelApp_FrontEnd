2aa21490f88eb1c8d2a2e07d5517dbb5f7cb51c8
Rakshit Nautiyal <rakshit.nautiyal41@outlook.com>
2023-03-05 13:07:22 +0000

    login working

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,8 @@
+// logout button
+var loginButton = document.getElementById("loginButton");
+
 $(document).ready(function() {
+  verifyAccessToken();
 
   // REF - https://ilovedjango.com/django/rest-api-framework/authentication/tips/working-example-of-jwt-authentication-with-ajax-django-rest-framework/
   $("#login-form").submit(function (event) {
@@ -18,6 +22,7 @@ $(document).ready(function() {
             // store tokens in localStorage
             window.localStorage.setItem('refreshToken', data['refresh']);
             window.localStorage.setItem('accessToken', data['access']);
+            window.location.replace("http://127.0.0.1:5501/index.html");
             console.log("success!!");
         },
         error: function (rs, e) {
@@ -26,4 +31,56 @@ $(document).ready(function() {
         }
     }); // end ajax
   });
+
+  function verifyAccessToken() {
+    $.ajax({
+      url: "http://127.0.0.1:8000/account/api/login/verify/",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify({ token: window.localStorage.getItem('accessToken')}),
+      success: function(data) {
+        // Handle success response
+        console.log(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        if (jqXHR.status === 401) {
+          // Access token expired, get a new token using the refresh token
+          $.ajax({
+            url: 'http://127.0.0.1:8000/account/api/login/refresh/',
+            type: 'POST',
+            data: {
+              refresh_token: window.localStorage.getItem('refreshToken')
+            },
+            dataType: 'json',
+            success: function(response) {
+              window.localStorage.setItem('accessToken', response.access_token);
+              window.localStorage.setItem('refreshToken', response.refresh_token);
+              // Make a request using the new access token
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+              console.log('Error getting new token:', errorThrown);
+            }
+          });
+        } else {
+            console.log('Error verifying access token:', errorThrown);
+            if(getCurrentURL() != "http://127.0.0.1:5501/login.html" ){
+              window.location.replace("http://127.0.0.1:5501/login.html");
+            }
+        }
+      }
+    });
+  }
+
+  function getCurrentURL () {
+    return window.location.href
+  }
+
+  loginButton.addEventListener("click", function() {
+    window.localStorage.removeItem('refreshToken');
+    window.localStorage.removeItem('accessToken');
+    window.location.replace("http://127.0.0.1:5501/login.html");
+    console.log("Button clicked!");
+  });
+
+
 });
